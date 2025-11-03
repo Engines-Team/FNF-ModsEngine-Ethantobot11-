3caf22d21a4213c8238c5077e10d240590b6391c
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-11-03 15:19:34 +0100

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -768,7 +768,7 @@ class PlayState extends MusicBeatState
 	}
 
 	public function reloadTimeBarColor() {
-		if ((FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]) = 0xFF000000));
+		if (FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]) == 0xFF000000),
 		{
 			timeBar.color = 0xFFFFFFFF;
 		} else 
@@ -778,7 +778,7 @@ class PlayState extends MusicBeatState
 	}
 
 	public function reloadHUDColor() {
-		if ((FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]) = 0xFF000000));
+		if (FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]) == 0xFF000000)
 		{
 			timeTxt.color = 0xFFFFFFFF;
 			timeBar.color = 0xFFFFFFFF;
